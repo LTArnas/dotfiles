7ad1c9d38c55d63b26236cd9ce6a0db5dffd5bc6
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2017-12-31 21:54:01 +0000

    Refacot t ouse .emacs.d directory setup.

---
+++ emacs/.emacs.d/arnas/init-evil-mode.el
@@ -0,0 +1,10 @@
+;;; .init.el -- My evil mode config.
+;;; Commentary:
+;;; Code:
+
+(use-package evil
+  :ensure t ; Install if not installed.
+  :config
+  (evil-mode 1))
+
+(provide 'init-evil-mode)
