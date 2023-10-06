cab18da4ac7cf70963afbaf460f3377c07ce30d6
Arnoldas Rudzevicius <arnoldas.rudzevicius@intelligentreach.com>
2023-10-06 23:24:40 +0100

    Improve nvim config - pyright lsp

--- configs/home/arnas/.config/nvim/lua/arnas/plugins/nvim_lspconfig.lua
+++ configs/home/arnas/.config/nvim/lua/arnas/plugins/nvim_lspconfig.lua
@@ -18,13 +18,42 @@ return {
       end)
 
       require('mason-lspconfig').setup({
-        ensure_installed = {"lua_ls"},
+        ensure_installed = {"lua_ls", "pyright"},
+        -- :h mason-lspconfig.setup_handlers()
         handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
+          lsp_zero.default_setup, -- default handler.
+          ["lua_ls"] = function()
             -- (Optional) Configure lua language server for neovim
             local lua_opts = lsp_zero.nvim_lua_ls()
             require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+          ["pyright"] = function()
+              local util = require("lspconfig/util")
+              local path = util.path
+              local function get_python_path(workspace)
+                  -- Use activated venv.
+                  if vim.env.VIRTUAL_ENV then
+                      return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+                  end
+
+                  -- Find and use virtualenv in workspace directory.
+                  --[[
+                  for _, pattern in ipairs({'*', '.*'}) do
+                    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+                    if match ~= '' then
+                      return path.join(path.dirname(match), 'bin', 'python')
+                    end
+                  end
+                  --]]
+
+                  -- Fallback to system Python.
+                  return exepath('python3') or exepath('python') or 'python'
+              end
+              require("lspconfig").pyright.setup({
+                  before_init = function(_, config)
+                      config.settings.python.pythonPath = get_python_path(config.root_dir)
+                  end,
+              })
           end,
         }
       })
