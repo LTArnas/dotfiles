7b890ffb9ac9050a5c51c4b44b08cf74903f7cea
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2023-10-26 22:25:44 +0100

    Add devicons. Add DAP mappings. Improve Telescope, incl. add fzf extension. More.

--- configs/home/arnas/.config/nvim/lua/arnas/plugins/nvim_dap.lua
+++ configs/home/arnas/.config/nvim/lua/arnas/plugins/nvim_dap.lua
@@ -1,7 +1,96 @@
 return {
     "mfussenegger/nvim-dap",
     config = function()
+        require("dap").defaults.fallback.exception_breakpoints = { "uncaught" }
+
         -- Load up ./.vscode/launch.json file, so we have those too. see :h dap-launch.json
         require("dap.ext.vscode").load_launchjs()
+
+        -- keymap
+        local kset = vim.api.nvim_set_keymap
+        kset("n", "<leader>dd", "", {
+            desc = "Debug: Start/Continue",
+            callback = function()
+                require("dap").continue()
+            end,
+        })
+        kset("n", "<leader>dr", "", {
+            desc = "Debug: Restart",
+            callback = function()
+                require("dap").restart()
+            end,
+        })
+        kset("n", "<leader>dt", "", {
+            desc = "Debug: Terminate",
+            callback = function()
+                require("dap").terminate()
+            end,
+        })
+        kset("n", "<leader>db", "", {
+            desc = "Debug: Breakpoint toggle",
+            callback = function()
+                require("dap").toggle_breakpoint()
+            end,
+        })
+        kset("n", "<leader>dbs", "", {
+            desc = "Debug: Breakpoint Set",
+            callback = function()
+                require("dap").set_breakpoint()
+            end,
+        })
+        kset("n", "<leader>dbl", "", {
+            desc = "Debug: Breakpoint list",
+            callback = function()
+                require("dap").list_breakpoints()
+            end,
+        })
+        kset("n", "<leader>dbc", "", {
+            desc = "Debug: Breakpoints Clear (all)",
+            callback = function()
+                require("dap").clear_breakpoints()
+            end,
+        })
+        kset("n", "<leader>dbe", "", {
+            desc = "Debug: Breakpoint set Exception version",
+            callback = function()
+                require("dap").set_exception_breakpoints()
+            end,
+        })
+        kset("n", "<leader>dso", "", {
+            desc = "Debug: Step Over",
+            callback = function()
+                require("dap").step_over()
+            end,
+        })
+        kset("n", "<leader>dsi", "", {
+            desc = "Debug: Step Into",
+            callback = function()
+                require("dap").step_into()
+            end,
+        })
+        kset("n", "<leader>dsu", "", {
+            desc = "Debug: Step out (Up)",
+            callback = function()
+                require("dap").step_out()
+            end,
+        })
+        kset("n", "<leader>dsb", "", {
+            desc = "Debug: Step Back",
+            callback = function()
+                require("dap").step_back()
+            end,
+        })
+        kset("n", "<leader>dp", "", {
+            desc = "Debug: Pause (thread)",
+            callback = function()
+                require("dap").pause()
+            end,
+        })
+        kset("n", "<leader>dp", "", {
+            desc = "Debug: Pause (thread)",
+            callback = function()
+                require("dap").pause()
+            end,
+        })
     end,
 }
--- configs/home/arnas/.config/nvim/lua/arnas/plugins/telescope.lua
+++ configs/home/arnas/.config/nvim/lua/arnas/plugins/telescope.lua
@@ -1,13 +1,48 @@
 return {
-    'nvim-telescope/telescope.nvim', 
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
     config = function(_, opts)
-	local builtin = require("telescope.builtin")
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-	vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+        require("telescope").setup()
+
+        require("telescope").load_extension("fzf")
+
+        local builtin = require("telescope.builtin")
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>fF",
+            "",
+            { desc = "(Telescope) Find: Files (find_files)", callback = builtin.find_files }
+        )
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>fg",
+            "",
+            { desc = "(Telescope) Find: Grep text in files (live_grep)", callback = builtin.live_grep }
+        )
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>fb",
+            "",
+            { desc = "(Telescope) Find: Buffers (buffers)", callback = builtin.buffers }
+        )
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>fh",
+            "",
+            { desc = "(Telescope) Find: Help docs (help_tags)", callback = builtin.help_tags }
+        )
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>fk",
+            "",
+            { desc = "(Telescope) Find: Keymaps (keymaps)", callback = builtin.keymaps }
+        )
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>ff",
+            "",
+            { desc = "(Telescope) Find: Git Files (git_files)", callback = builtin.git_files }
+        )
     end,
 }
